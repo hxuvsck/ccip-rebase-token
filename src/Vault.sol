69b4548e6af0c788f5fc289a5971e080446b9bb0
hxuvsck <hxuvsck@gmail.com>
2026-04-26 15:06:31 +0800

    Natspec and emit,error in vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -29,29 +29,56 @@ pragma solidity ^0.8.20;
  * @notice
  */
 
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+
 contract Vault {
     // We need to pass the token address to the constructor
     // Create a deposit function that mints tokens to the user equal to the amount of ETH has sent
     // Create a redeem function that burns tokens from the user and sends the user ETH
     // Create a way to add rewards to the vault
 
-    address private immutable i_rebaseToken;
+    IRebaseToken private immutable i_rebaseToken;
 
     event Deposit(address indexed user, uint256 amount);
+    event Redeem(address indexed user, uint256 amount);
 
-    constructor(address _rebaseToken) {
+    error Vault__RedeemFailed();
+
+    constructor(IRebaseToken _rebaseToken) {
         i_rebaseToken = _rebaseToken;
     }
 
     receive() external payable {}
 
+    /**
+     * @notice Allows users to deposit ETH into the vault and mint rebase tokens in return
+     */
     function deposit() external payable {
         // 1. We need to use the amount of ETH the user has sent to mint tokens to the user
         i_rebaseToken.mint(msg.sender, msg.value);
         emit Deposit(msg.sender, msg.value);
     }
 
+    /**
+     * @notice Allows users to redeem their rebase tokens for ETH
+     * @param _amount The amount of rebase tokens to redeem
+     */
+    function redeem(uint256 _amount) external {
+        // 1. Burn the token from the user
+        i_rebaseToken.burn(msg.sender, _amount);
+        // 2. We need to send the user ETH
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender, _amount);
+    }
+
+    /**
+     * @notice Get the address of the rebase token
+     * @return The address of the rebase token
+     */
     function getRebaseTokenAddress() external view returns (address) {
-        return i_rebaseToken;
+        return address(i_rebaseToken);
     }
 }
