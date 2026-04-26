72e8a01208e5ac523436ff3eca3edc2539e09666
hxuvsck <hxuvsck@gmail.com>
2026-04-26 14:31:19 +0800

    Payable 2 function add to Vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -37,11 +37,19 @@ contract Vault {
 
     address private immutable i_rebaseToken;
 
+    event Deposit(address indexed user, uint256 amount);
+
     constructor(address _rebaseToken) {
         i_rebaseToken = _rebaseToken;
     }
 
     receive() external payable {}
+
+    function deposit() external payable {
+        // 1. We need to use the amount of ETH the user has sent to mint tokens to the user
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
 
     function getRebaseTokenAddress() external view returns (address) {
         return i_rebaseToken;
