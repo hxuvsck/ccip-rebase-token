e4f4377b5e8147ac485dd3872fd538d00dee75d3
hxuvsck <hxuvsck@gmail.com>
2026-05-25 21:08:02 +0800

    Test cross chain fork simulation with chainlink local initiate pass

--- test/CrossChainTest.t.sol
+++ test/CrossChainTest.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 
+import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {Vault} from "../src/Vault.sol";
@@ -13,8 +15,14 @@ import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 contract CrossChainTest is Test {
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
+
+    CCIPLocalSimulatorFork simulator;
+
     function setUp() public {
         sepoliaFork = vm.createSelectFork("sepolia-eth");
         arbSepoliaFork = vm.createFork("arb-sepolia");
+
+        simulator = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(simulator));
     }
 }
